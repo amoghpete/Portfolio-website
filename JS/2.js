ce22d547ced8d59983a0f991fb20a4e6167c6f8e
Amogh Pete <amoghpete@gmail.com>
2020-10-29 17:46:57 +0530

    First Phase done

--- JS/2.js
+++ JS/2.js
@@ -1,26 +1,23 @@
+window.onload = function () {
+  var triangle = document.getElementById("triangle");
+  var length = triangle.getTotalLength();
 
-var triangle = document.getElementById("triangle");
-var length = triangle.getTotalLength();
+  triangle.style.strokeDasharray = length;
 
+  triangle.style.strokeDashoffset = length;
 
-triangle.style.strokeDasharray = length;
+  window.addEventListener("scroll", myFunction);
 
+  function myFunction() {
+    var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight -1200);
+    
+    var draw = length * scrollpercent;
 
-triangle.style.strokeDashoffset = length;
-
-
-window.addEventListener("scroll", myFunction);
-
-function myFunction() {
-var scrollpercent = (document.body.scrollTop + document.documentElement.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-
-  var draw = length * scrollpercent;
+    triangle.style.strokeDashoffset = length - draw;
+  }
+};
   
- 
-  triangle.style.strokeDashoffset = length - draw;
+function transit() {
+  var element = document.getElementById("toggle");
+  element.classList.toggle("anim-trans");
 }
-
-
-$( ".click" ).click(function() {
-  $( ".transition").toggleClass( "anim-trans" );
-});
